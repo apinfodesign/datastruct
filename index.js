51173c5cd948c632c8301ef3e61f31cf4a3b8ea0
Miles Hochstein <miles.hochstein@gmail.com>
2016-01-11 21:53:01 -0800

    frequentLetter now works with mocha tests

--- index.js
+++ index.js
@@ -5,45 +5,36 @@
 
 
 	function push(array, invalue){
-		console.log(array);
-		console.log('invalue is ', invalue);
-		var arraynext=[];
+ 		var arraynext=[];
 		for(var i=0; i<array.length; i++){
 			arraynext[i+1] = array[i];}
 		arraynext[0]=invalue;
 		array = arraynext;
-	 	console.log(array);
- 		return array;
+  		return array;
 	};
 
 	function pop(array){
 		var popvalue = array[0];
-		console.log('doing pop on array ',array);
+		//console.log('doing pop on array ',array);
 		for(var i=0; i<array.length; i++){
  			array[i]=array[i+1];
 		}
 		var array = array.slice(0, array.length-1);
-		console.log('after pop array is: ', array);
-		console.log('we are popping ', popvalue);
  		return popvalue;//position 0 value
 	};
 
 	function shift(array, invalue){
 	 	array[array.length]= invalue;
-		console.log("shift array is ", array);
 		return array;
  	};
 
 	function unshift(array){
 		unshiftvalue = array[array.length-1];
 		var array = array.slice(0,array.length-1);
-		console.log("after unshift array is ", array);
-		console.log('we are unshifting ', unshiftvalue);
 		return unshiftvalue;
  	};
 
 	function unique(array){
-		console.log("redundant array", array);
  		var uniqueArray=[];
 		for(var i=0;i<array.length;i++){
 			//indexOf is -1 if redundant  			
@@ -51,32 +42,85 @@
  				uniqueArray.push(array[i]);
  			}    	 
   		}
-  		console.log("unique array :",uniqueArray);
    		return uniqueArray;
-	}
- 
-//for each letter of alphabet
-//check if letter appears
-//	function commonletter(){
-		//cycle through array 
-		//for each word
-///	};
+	};
 
+
+	function frequentLetter(array){
+ 	 	//var mostFrequent = 'temp';
+ 	 	var ArrayOfUniqueLetters = [];
+		for(var i=0;i<array.length;i++){
+			//make an array from each string			
+			reductionArray = array[i].split('')
+			//array to unique array (shorten sometimes)
+			uniqueArray = unique(reductionArray);
+ 			//letter count to letter collector
+			ArrayOfUniqueLetters = ArrayOfUniqueLetters.concat(uniqueArray); 
+ 		}
+
+ 		//console.log(ArrayOfUniqueLetters);
+
+ 		//count repeats in a sorted array
+       	var blob = {letter:"", count:"" };  //make object for later sorting
+    	var count = 1;  //start count at 1 in case first compare not equal 
+    	var accumulator=[];  
+	   	ArrayOfUniqueLetters.sort(); //squish same letters together
+		for(var i=0;i<ArrayOfUniqueLetters.length;i++){
+ 			if (ArrayOfUniqueLetters[i] === ArrayOfUniqueLetters[i+1])
+				{count=count+1; 
+ 			}
+			else{
+				//set blob count to last count increase plus one
+				blob ={ letter: ArrayOfUniqueLetters[i], count: count+1};
+  				count=0; //reset count for next letter
+ 				accumulator.push(blob);
+ 			}
+		}
+ 
+		//function to sort on numerical value -  not ideal location
+				function compare(a,b) {
+				  if (a.count < b.count)
+				    return -1;
+				  else if (a.count> b.count)
+				    return 1;
+				  else 
+				    return 0;
+				}
+
+		accumulator.sort(compare); //sort on count 
+		//console.log("accumulator is: ", accumulator);
+		finalResult = unshift ( accumulator.sort(compare) );
+
+		// console.log(finalResult);
+		// console.log(finalResult.letter);
+		// console.log(finalResult.count);
+
+		console.log("Letter in largest number of words is: ", finalResult.letter, 
+					" appearing ", finalResult.count, " times.")
+		return finalResult.letter; 
+}
+
+  
 // unique(array1);
 // push(array1,'horse');
 // pop(array1);
 // shift(array1, 'cow');
 // unshift(array1);
-// console.log(pop(array1) );
 
-//console.log(unique(array1));
+//additional test arrays for frequentLetter 
+var array8=['apple','lemon','book','lemon','dog']; //answer o
+var array9=['appleeeeen','lemon','bookn','lemooooon','dogn']; //answer o
+var array10=['applen','lemon','bookn','lemonn','dogn']; //answer n
+var array11=['after','back','cat','dad','eeeeeee','rrr','ggg']; //answer a
 
+//frequentLetter(array10);
 
 module.exports = {
 	push: push, 
 	pop: pop,
 	shift: shift,
 	unshift: unshift,
-	unique: unique 
+	unique: unique,
+	frequentLetter: frequentLetter
 }
  
--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 //test.js
 var assert = require('assert'); 
 var arraystuff = require('./index.js');
-var array1=['apple','lemon','house','lemon','dog'];
-var array2=['apple','lemon','house','lemon','dog'];
-var array3=['apple','lemon','house','lemon','dog'];
-var array4=['apple','lemon','house','lemon','dog'];
-var array5=['apple','lemon','house','lemon','dog'];
-var array6=['apple','lemon','house','lemon','dog'];
+
+//each test starts with a fresh unmodified version of testing array
+var array1=['apple','lemon','book','lemon','dog'];
+var array2=['apple','lemon','book','lemon','dog'];
+var array3=['apple','lemon','book','lemon','dog'];
+var array4=['apple','lemon','book','lemon','dog'];
+var array5=['apple','lemon','book','lemon','dog'];
+var array6=['apple','lemon','book','lemon','dog'];
+var array7=['apple','lemon','book','lemon','dog'];
+var array8=['apple','lemon','book','lemon','dog']; //answer o
+var array9=['appleeeeen','lemon','bookn','lemooooon','dogn']; //answer o
+var array10=['applen','lemon','bookn','lemonn','dogn']; //answer n
+var array11=['after','back','cat','dad','eeeeeee','rrr','ggg']; //answer a
 
 
-describe(' Data Structures / Algorithms Module Tests ', function(){
+describe('Data Structures & Algorithms Tests ', function(){
 
 	it('should do nothing ', function(done){
-			console.log('test 1');
- 	 		assert.deepEqual(
+  	 		assert.deepEqual(
  	 			arraystuff.push(array1, 'bread'),  
  	 			arraystuff.push(array1, 'bread')
  	 		)
@@ -21,18 +27,15 @@ describe(' Data Structures / Algorithms Module Tests ', function(){
 	});
 
 	it('should do a push operation ', function(done){
-			console.log('test 2');
-
- 	 		assert.deepEqual(
+  	 		assert.deepEqual(
  	 			arraystuff.push(array2, 'bread'),  
- 	 			['bread', 'apple','lemon','house','lemon','dog']
+ 	 			['bread', 'apple','lemon','book','lemon','dog']
  	 		)
 			done();
 	});
    	 
 	it('should do a pop operation ', function(done){
-			console.log('test 3');
- 	 		assert.deepEqual(
+  	 		assert.deepEqual(
  	 			arraystuff.pop(array3),  
  	 			'apple'
  	 		)
@@ -40,19 +43,15 @@ describe(' Data Structures / Algorithms Module Tests ', function(){
 	});
 
 	it('should do a shift operation ', function(done){
-			console.log('test 4');
-
- 	 		assert.deepEqual(
+  	 		assert.deepEqual(
  	 			arraystuff.shift(array4, 'cow'),  
- 	 			['apple','lemon','house','lemon','dog','cow']
+ 	 			['apple','lemon','book','lemon','dog','cow']
  	 		)
 			done();
 	});
 
 
 	it('should do an unshift operation ', function(done){
-			console.log('test 5');
-
  	 		assert.deepEqual(
  	 			arraystuff.unshift(array5),  
  	 			'dog'
@@ -61,11 +60,19 @@ describe(' Data Structures / Algorithms Module Tests ', function(){
 	});
 
 	it('should delete any redundant elements ', function(done){
-			console.log('test 6');
-
  	 		assert.deepEqual(
  	 			arraystuff.unique(array6),  
- 	 			['apple','lemon','house','dog'] 	 		)
+ 	 			['apple','lemon','book','dog']
+ 	 		)
 			done();
 	});
+
+	it('should return letter in highest number of words', function(done){
+ 	 		assert.deepEqual(
+ 	 			arraystuff.frequentLetter(array8),  
+ 	 			'o'
+ 	 		)
+			done();
+	});
+
 });
